91259df921520f47b8b46f26f0a31f1cda77bda3
Gaston Segura <gastonsegura2908@mi.unc.edu.ar>
2024-10-02 08:49:05 -0300

    fix nombres de variables en rx_module

--- TP2-UART/TP2-UART.srcs/sources_1/new/rx_module.v
+++ TP2-UART/TP2-UART.srcs/sources_1/new/rx_module.v
@@ -41,9 +41,9 @@ localparam [1:0]
 
 // signal declaration 
 reg [1:0] rxmodule_statereg ,    rxmodule_nextstatereg ;    // 2 bits ya que hay 4 estados, de 0 a 3
-reg [3:0] rxmodule_samptickreg , rxmodule_sampticknextreg ; // 4 bits ya que hay que contar 16 bits, de 0 a 15
-reg [2:0] rxmodule_nbrecreg ,    rxmodule_nbrecnextreg ;    // 3 bits ya que, son maximo 8: de 0 a 7
-reg [7:0] rxmodule_bitsreasreg , rxmodule_bitsreasnextreg ; // 8 bits ya que esa es la cantidad de bits que manejamos
+reg [3:0] rxmodule_samptickreg , rxmodule_nextsamptickreg ; // 4 bits ya que hay que contar 16 bits, de 0 a 15
+reg [2:0] rxmodule_nbrecreg ,    rxmodule_nextnbrecreg ;    // 3 bits ya que, son maximo 8: de 0 a 7
+reg [7:0] rxmodule_bitsreasreg , rxmodule_nextbitsreasreg ; // 8 bits ya que esa es la cantidad de bits que manejamos
 
 // body
 // FSMD state & rxmodule_datastate registers 
@@ -58,9 +58,9 @@ always @( posedge i_clk , posedge i_reset)
    else 
       begin 
          rxmodule_statereg    <= rxmodule_nextstatereg ; 
-         rxmodule_samptickreg <= rxmodule_sampticknextreg; 
-         rxmodule_nbrecreg    <= rxmodule_nbrecnextreg; 
-         rxmodule_bitsreasreg <= rxmodule_bitsreasnextreg; 
+         rxmodule_samptickreg <= rxmodule_nextsamptickreg; 
+         rxmodule_nbrecreg    <= rxmodule_nextnbrecreg; 
+         rxmodule_bitsreasreg <= rxmodule_nextbitsreasreg; 
       end 
 
 // FSMD next-state logic 
@@ -68,16 +68,16 @@ always @(*)
    begin 
       o_rxmodule_RXDONE        = 1'b0;                   // salida en 0
       rxmodule_nextstatereg    = rxmodule_statereg ;     // el siguiente estado sera el mismo que el actual, excepto algunos casos, que son los que cumplen la cant de ticks
-      rxmodule_sampticknextreg = rxmodule_samptickreg; 
-      rxmodule_nbrecnextreg    = rxmodule_nbrecreg; 
-      rxmodule_bitsreasnextreg = rxmodule_bitsreasreg; 
+      rxmodule_nextsamptickreg = rxmodule_samptickreg; 
+      rxmodule_nextnbrecreg    = rxmodule_nbrecreg; 
+      rxmodule_nextbitsreasreg = rxmodule_bitsreasreg; 
 
       case (rxmodule_statereg) // estado actual
          rxmodule_idlestate : 
             if (~i_rxmodule_RX) // el bit de start en uart(es un 0)
                begin 
                   rxmodule_nextstatereg    = rxmodule_startstate ;   // SIGUIENTE ESTADO
-                  rxmodule_sampticknextreg = 0; 
+                  rxmodule_nextsamptickreg = 0; 
                end 
          
          rxmodule_startstate : 
@@ -85,25 +85,25 @@ always @(*)
                if (rxmodule_samptickreg == 7)   // (8 ticks) mitad del bit start --|_+_|--
                   begin 
                      rxmodule_nextstatereg    = rxmodule_datastate;  // SIGUIENTE ESTADO
-                     rxmodule_sampticknextreg = 0; 
-                     rxmodule_nbrecnextreg    = 0; 
+                     rxmodule_nextsamptickreg = 0; 
+                     rxmodule_nextnbrecreg    = 0; 
                   end 
                else 
-                  rxmodule_sampticknextreg = rxmodule_samptickreg + 1;  // seguimos sumando ticks
+                  rxmodule_nextsamptickreg = rxmodule_samptickreg + 1;  // seguimos sumando ticks
          
          rxmodule_datastate : 
             if (i_rxmodule_BRGTICKS) 
                if (rxmodule_samptickreg == 15)  // (16 ticks) mitad del dato.
                   begin 
-                     rxmodule_sampticknextreg = 0; 
-                     rxmodule_bitsreasnextreg = {i_rxmodule_RX , rxmodule_bitsreasreg [7:1]}; 
+                     rxmodule_nextsamptickreg = 0; 
+                     rxmodule_nextbitsreasreg = {i_rxmodule_RX , rxmodule_bitsreasreg [7:1]}; 
                      if (rxmodule_nbrecreg == (NB_RXMODULE_DATA - 1))   // llegaron los 8 bits
                         rxmodule_nextstatereg = rxmodule_stopstate ;    // SIGUIENTE ESTADO
                      else 
-                        rxmodule_nbrecnextreg = rxmodule_nbrecreg + 1;  // seguimos sumando bits recibidos
+                        rxmodule_nextnbrecreg = rxmodule_nbrecreg + 1;  // seguimos sumando bits recibidos
                   end 
             else 
-               rxmodule_sampticknextreg = rxmodule_samptickreg + 1;     // seguimos sumando ticks
+               rxmodule_nextsamptickreg = rxmodule_samptickreg + 1;     // seguimos sumando ticks
          
          rxmodule_stopstate: 
             if (i_rxmodule_BRGTICKS) 
@@ -113,7 +113,7 @@ always @(*)
                   o_rxmodule_RXDONE = 1'b1;                              // se pone en 1 la salida cuando los 8 bits son obtenidos,desde RX
                end 
                else 
-                  rxmodule_sampticknextreg = rxmodule_samptickreg + 1; // seguimos sumando ticks
+                  rxmodule_nextsamptickreg = rxmodule_samptickreg + 1; // seguimos sumando ticks
       endcase 
    end 
 
