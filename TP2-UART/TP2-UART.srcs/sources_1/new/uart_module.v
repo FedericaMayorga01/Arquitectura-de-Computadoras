dbd21ef5eab72c0d1db1c883847ec6eef8f9577a
Federica <federica.mayorga@mi.unc.edu.ar>
2024-10-03 18:04:07 -0300

    Refactor uart_module.v in TP2-UART

--- TP2-UART/TP2-UART.srcs/sources_1/new/uart_module.v
+++ TP2-UART/TP2-UART.srcs/sources_1/new/uart_module.v
@@ -5,25 +5,40 @@ module uart_module
     parameter NB_UARTMODULE_DATA    = 8,    // data bits
               SB_UARTMODULE_TICKS   = 16,   // stop bits ticks
               NB_UARTMODULE_COUNTER = 9,    // counter bits
-              MOD_UARTMODULE_M      = 325   // ms counter bits
+              MOD_UARTMODULE_M      = 325,  // ms counter bits
+              NB_UARTMODULE_ADDR    = 4     // address bits
 )
 (
-    input   wire    i_clk, i_reset,
+    input  wire    i_clk, i_reset,
 
     // receiver port
-    input   wire    i_uartmodule_RX,
-    output  wire    o_uartmodule_RXDONE,
-    output  wire    [7:0] o_uartmodule_DOUT,
+    input  wire    i_uartmodule_RX,
 
     // transmitter port
-    input   wire    i_uartmodule_TXSTART,
-    input   wire    [7:0] i_uartmodule_DIN,
-    output  wire    o_uartmodule_TXDONE,
-    output  wire    o_uartmodule_TX
+    output wire    o_uartmodule_TX,
+
+    // FIFO RX ports
+    input  wire i_uartmodule_fiforx_READ,
+    output wire i_uartmodule_fiforx_READDATA,
+    output wire o_uartmodule_fiforx_fifo_EMPTY,
+
+    // FIFO TX ports
+    input  wire i_uartmodule_fifotx_WRITEDATA,
+    input  wire i_uartmodule_fifotx_WRITE,
+    output wire o_uartmodule_fifotx_fifo_FULL
+
 );
 
 // Señal interna para el baud rate generator
 wire uartmodule_maxtick;
+// Señal interna para el UART Rx
+wire  o_uartmodule_rxdone;
+wire  [7:0] o_uartmodule_dout;
+// Señal interna para el UART Tx
+wire  o_uartmodule_txdone;
+// Señal interna para el FIFO TX
+wire  o_uartmodule_readdata;
+wire  o_uartmodule_fifo_empty;
 
 //--------------- INICIALIZACION DE MODULOS --- start
 
@@ -37,8 +52,7 @@ baudrg_module #(
     .o_baudrgmodule_RATE()                        // No se usa momentaneamente
 );
 
-rx_module
-#(
+rx_module #(
     .NB_RXMODULE_DATA(NB_UARTMODULE_DATA),
     .SB_RXMODULE_TICKS(SB_UARTMODULE_TICKS)
 ) rx_module_1 (
@@ -46,24 +60,50 @@ rx_module
     .i_reset(i_reset),
     .i_rxmodule_RX(i_uartmodule_RX),
     .i_rxmodule_BRGTICKS(uartmodule_maxtick),
-    .o_rxmodule_RXDONE(o_uartmodule_RXDONE),
-    .o_rxmodule_DOUT(o_uartmodule_DOUT)
+    .o_rxmodule_RXDONE(o_uartmodule_rxdone),
+    .o_rxmodule_DOUT(o_uartmodule_dout)
 );
 
-tx_module
-#(
+fifo_rx_module #(
+    .NB_FIFOMODULE_DATA(NB_UARTMODULE_DATA),
+    .NB_FIFOMODULE_ADDR(NB_UARTMODULE_ADDR)
+) fifo_rx_module_1 (
+    .i_clk(i_clk),
+    .i_reset(i_reset),
+    .i_fiforxmodule_read(i_uartmodule_fiforx_READ),
+    .i_fiforxmodule_write(o_uartmodule_rxdone),
+    .i_fiforxmodule_writedata(o_uartmodule_dout),
+    .o_fiforxmodule_fifo_empty(o_uartmodule_fiforx_fifo_EMPTY),
+    .o_fiforxmodule_fifo_full(),
+    .o_fiforxmodule_readdata(i_uartmodule_fiforx_READDATA)
+);
+
+tx_module #(
     .NB_TXMODULE_DATA(NB_UARTMODULE_DATA),
     .SB_TXMODULE_TICKS(SB_UARTMODULE_TICKS)
 ) tx_module_1 (
     .i_clk(i_clk),
     .i_reset(i_reset),
-    .i_txmodule_TXSTART(i_uartmodule_TXSTART),
+    .i_txmodule_TXSTART(~o_uartmodule_fifo_empty),
     .i_txmodule_BRGTICKS(uartmodule_maxtick),
-    .i_txmodule_DIN(i_uartmodule_DIN),
-    .o_txmodule_TXDONE(o_uartmodule_TXDONE),
+    .i_txmodule_DIN(o_uartmodule_readdata),
+    .o_txmodule_TXDONE(o_uartmodule_txdone),
     .o_txmodule_TX(o_uartmodule_TX)
 );
 
+fifo_tx_module #(
+    .NB_FIFOMODULE_DATA(NB_UARTMODULE_DATA),
+    .NB_FIFOMODULE_ADDR(NB_UARTMODULE_ADDR)
+) fifo_tx_module_1 (
+    .i_clk(i_clk),
+    .i_reset(i_reset),
+    .i_fifotxmodule_read(o_uartmodule_txdone),
+    .i_fifotxmodule_write(i_uartmodule_fifotx_WRITE),
+    .i_fifotxmodule_writedata(i_uartmodule_fifotx_WRITEDATA),
+    .o_fifotxmodule_fifo_empty(o_uartmodule_fifo_empty),
+    .o_fifotxmodule_fifo_full(o_uartmodule_fifotx_fifo_FULL),
+    .o_fifotxmodule_readdata(o_uartmodule_readdata)
+);
 
 //--------------- INICIALIZACION DE MODULOS --- end
 
