120ac23c0bc97295d378ee6b5618c08c7d407295
Federica <federica.mayorga@mi.unc.edu.ar>
2024-10-02 23:18:36 -0300

    Refactor the i/o for uart_module.v in TP2-UART

--- TP2-UART/TP2-UART.srcs/sources_1/new/uart_module.v
+++ TP2-UART/TP2-UART.srcs/sources_1/new/uart_module.v
@@ -3,24 +3,41 @@
 module uart_module
 #(
     parameter NB_UARTMODULE_DATA    = 8,    // data bits
-              SB_UARTMODULE_TICKS   = 16,   // ticks bits
+              SB_UARTMODULE_TICKS   = 16,   // stop bits ticks
               NB_UARTMODULE_COUNTER = 9,    // counter bits
               MOD_UARTMODULE_M      = 325   // ms counter bits
 )
 (
     input   wire    i_clk, i_reset,
-    input   wire    i_uartmodule_BRGTICKS,
-    input   wire    i_uartmodule_RX,
-    input   wire    i_uartmodule_TXSTART,       // ver si estan bien los bits en este
-    input   wire    [7:0] i_uartmodule_DIN,     // ver si estan bien los bits en este
 
+    // baud rate generator
+    input   wire    i_uartmodule_BRGTICKS,
+
+    // receiver port
+    input   wire    i_uartmodule_RX,
     output  wire    o_uartmodule_RXDONE,
     output  wire    [7:0] o_uartmodule_DOUT,
+
+    // transmitter port
+    input   wire    i_uartmodule_TXSTART,       // ver si estan bien los bits en este
+    input   wire    [7:0] i_uartmodule_DIN,     // ver si estan bien los bits en este
     output  wire    o_uartmodule_TXDONE,
     output  wire    o_uartmodule_TX
 );
 
 //--------------- INICIALIZACION DE MODULOS --- start
+
+baudrg_module #(
+    .NB_BAUDRGMODULE_COUNTER(NB_UARTMODULE_COUNTER),
+    .MOD_BAUDRGMODULE_M(MOD_UARTMODULE_M)
+) baudrg_module_1 (
+    .i_clk(i_clk),
+    .i_reset(i_reset),
+    .o_baudrgmodule_MAXTICK(),// VER ESTOO
+    .o_baudrgmodule_RATE() // VER ESTOO
+);
+
+
 rx_module
 #(
     .NB_RXMODULE_DATA(NB_UARTMODULE_DATA),
@@ -48,15 +65,7 @@ tx_module
     .o_txmodule_TX(o_uartmodule_TX)
 );
 
-baudrg_module #(
-    .NB_BAUDRGMODULE_COUNTER(NB_UARTMODULE_COUNTER),
-    .MOD_BAUDRGMODULE_M(MOD_UARTMODULE_M)
-) baudrg_module_1 (
-    .i_clk(i_clk),
-    .i_reset(i_reset),
-    .o_baudrgmodule_MAXTICK(),// VER ESTOO
-    .o_baudrgmodule_RATE() // VER ESTOO
-);
+
 //--------------- INICIALIZACION DE MODULOS --- end
 
 
